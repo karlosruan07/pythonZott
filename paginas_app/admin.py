b77ac2f70bf50f1884771116b422dfe14dd380f5
Ruan <karlosruan9@gmail.com>
2021-07-16 15:24:43 -0300

    adicionando gitignore

--- paginas_app/admin.py
+++ paginas_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Login_sistema, Materia
+from .models import Post, Login_sistema, Materia, Prova
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Login_sistema)
 admin.site.register(Materia)
+admin.site.register(Prova)
